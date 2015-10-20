968b6d07ba3bda2c1c86d78e49656bd8f369697e
Michael Bowman <bowmanmc@gmail.com>
2015-10-20 14:08:23 -0400

    Got the filter functions to work

--- src/modules/components/preview/glPreviewController.js
+++ src/modules/components/preview/glPreviewController.js
@@ -8,8 +8,21 @@ module.exports = function(ngModule) {
             console.log('glPreviewController initializing...');
 
             $scope.options = glOptionService;
-            $scope.brightness = 10;
-            $scope.contrast = 10;
+            $scope.brightness = 1.0;
+            $scope.contrast = 1.0;
+
+            $scope.opts = [
+                0.10, 0.25, 0.50, 0.75,
+                1.0, 1.5, 2.0, 3.0, 5.0,
+                10.0, 20.0, 50.0
+            ];
+
+            $scope.$watch('contrast', function() {
+                $scope.filter = `-webkit-filter: contrast(${$scope.contrast}) brightness(${$scope.brightness});`;
+            });
+            $scope.$watch('brightness', function() {
+                $scope.filter = `-webkit-filter: contrast(${$scope.contrast}) brightness(${$scope.brightness});`;
+            });
         };
 
         initialize();
